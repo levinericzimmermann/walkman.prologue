b9f076538d7b3bb9a942a7466d874439e50a01d9
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2022-06-22 23:21:23 +0200

    Use resonantors instead of convolution

--- walkman/walkman_modules.convolution_reverb_prologue/walkman_modules/convolution_reverbs/__init__.py
+++ walkman/walkman_modules.convolution_reverb_prologue/walkman_modules/convolution_reverbs/__init__.py
@@ -1,30 +1,116 @@
 from __future__ import annotations
 
+import typing
 
 import pyo
 
 import walkman
 
+# Please see https://cnmat.berkeley.edu/sites/default/files/patches/Picture%203_0.png
+Frequency = float
+Amplitude = float
+DecayRate = float
+ResonatorConfiguration = typing.Tuple[Frequency, Amplitude, DecayRate]
+FilePath = str
 
-class ConvolutionReverbPrologue(walkman.ConvolutionReverb):
+
+class ConvolutionReverbPrologue(
+    walkman.ModuleWithDecibel,
+    audio_input=walkman.Catch(walkman.constants.EMPTY_MODULE_INSTANCE_NAME),
+):
+    def __init__(
+        self,
+        resonance_configuration_file_path_list: typing.List[
+            typing.Tuple[Amplitude, FilePath]
+        ],
+        add_envelope_follower: bool = False,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.add_envelope_follower = add_envelope_follower
+        self.resonator_configuration_tuple = (
+            self.parse_resonance_configuration_file_path_list(
+                resonance_configuration_file_path_list
+            )
+        )
+
+    @staticmethod
+    def parse_resonance_configuration_file_path_list(
+        resonance_configuration_file_path_list: typing.List[
+            typing.Tuple[Amplitude, FilePath]
+        ]
+    ) -> typing.Tuple[
+        typing.Tuple[Amplitude, typing.Tuple[ResonatorConfiguration, ...]]
+    ]:
+        resonator_configuration_list = []
+        for (
+            amplitude,
+            resonance_configuration_file_path,
+        ) in resonance_configuration_file_path_list:
+            resonance_configuration_tuple = (
+                ConvolutionReverbPrologue.parse_resonance_configuration_file(
+                    resonance_configuration_file_path
+                )
+            )
+            resonator_configuration_list.append(
+                (amplitude, resonance_configuration_tuple)
+            )
+        return tuple(resonator_configuration_list)
+
+    @staticmethod
+    def parse_resonance_configuration_file(
+        resonance_configuration_file_path: str,
+    ) -> typing.Tuple[ResonatorConfiguration, ...]:
+        with open(
+            resonance_configuration_file_path, "r"
+        ) as resonance_configuration_file:
+            configuration_content = resonance_configuration_file.read()
+
+        resonance_configuration_list = []
+        for configuration_line in configuration_content.splitlines():
+            try:
+                _, data = configuration_line.replace(";", "").split(",")
+            except ValueError:
+                continue
+            frequency, bandwidth, amplitude = (
+                float(value) for value in filter(bool, data.split(" "))
+            )
+            resonance_configuration_list.append((frequency, bandwidth, amplitude))
+        # XXX: We need set because in the text files are duplicates.
+        return tuple(set(resonance_configuration_list))
+
     def _setup_pyo_object(self):
         super()._setup_pyo_object()
-        # del self.convolution_reverb
-        self.convolve = pyo.Convolve(
-            self.audio_input.pyo_object,
-            pyo.SndTable(self.impulse_path),
-            size=512,
-        )
-        self.balance_convolve = pyo.Balance(self.convolve, self.audio_input.pyo_object)
-        self.convolution_reverb.setInput(self.balance_convolve)
-        self.compressor = pyo.Compress(self.convolution_reverb, thresh=-15, ratio=3)
-        # To avoid crazy peaks
-        self.clip = pyo.Clip(self.compressor)
-        # self.internal_pyo_object_list.extend([self.compressor, self.clip])
-        self.internal_pyo_object_list.extend(
-            [self.convolve, self.compressor, self.clip, self.balance_convolve]
-        )
+        self.resonator_list = []
+        for (
+            amplitude,
+            resonance_configuration_tuple,
+        ) in self.resonator_configuration_tuple:
+            resonance_filter_list = [
+                pyo.ComplexRes(
+                    self.audio_input.pyo_object,
+                    decay=decay_rate,
+                    freq=frequency,
+                    mul=amplitude,
+                )
+                for frequency, amplitude, decay_rate in resonance_configuration_tuple
+            ]
+            resonator = sum(resonance_filter_list) * amplitude
+            self.resonator_list.append(resonator)
+            self.internal_pyo_object_list.extend(resonance_filter_list)
+
+        self.summed_resonator = sum(self.resonator_list) * self.amplitude_signal_to
+
+        internal_pyo_object_list = [self.summed_resonator] + self.resonator_list
+        if self.add_envelope_follower:
+            self.envelope_follower = pyo.Follower(self.audio_input.pyo_object)
+            # self.printer = pyo.Print(self.envelope_follower)
+            self.summed_resonator *= self.envelope_follower
+            internal_pyo_object_list.append(self.envelope_follower)
+
+        self.internal_pyo_object_list.extend(internal_pyo_object_list)
+        self._stop()
 
     @property
     def _pyo_object(self) -> pyo.PyoObject:
-        return self.clip
+        return self.summed_resonator
